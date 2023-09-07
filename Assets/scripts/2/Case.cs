a6bf0c39a35a99846fd911ea77b64d74bc00ddcb
LeonZhouZiang <leonzhouziang@gmail.com>
2023-09-07 19:31:33 -0400

    tmp
    tmp

---
+++ Assets/scripts/2/Case.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Case : Item
+{
+    public static bool flag;
+    void Start()
+    {
+        
+    }
+
+    public override void OnInteract()
+    {
+        
+    }
+}
