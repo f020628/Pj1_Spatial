d59c1cd00f046796c1b80156e5ff2f02e585552d
LeonZhouZiang <leonzhouziang@gmail.com>
2023-09-13 01:36:25 -0400

    final final
    final

--- Assets/scripts/CameraControl.cs
+++ Assets/scripts/CameraControl.cs
@@ -23,9 +23,8 @@ public class CameraControl : MonoBehaviour
     private void GetTarget()
     {
         Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width/2 , Screen.height/2));
-        Debug.DrawRay(ray.origin, ray.direction.normalized * 1.4f, Color.red);
         RaycastHit hit;
-        if(Physics.Raycast(ray, out hit, 1.4f, 1 << 6))
+        if(Physics.Raycast(ray, out hit, 1.6f, 1 << 6))
         {
             //Debug.Log(hit.collider.gameObject.GetComponent<Item>());
             if (hit.collider.gameObject.GetComponent<Item>() != null)
